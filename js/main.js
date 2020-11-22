1088a6876aa0ee538fb45355ec860474514b504f
JumrainiJamaluddin <jumrainijamaluddin@gmail.com>
2020-11-22 15:32:42 +0800

    permision Geolocation

--- js/main.js
+++ js/main.js
@@ -1,9 +1,32 @@
+ //nama function callback
+ //success
+ function success(position){
+     console.log(position);
+ }
+
+ //eror
+function eror(position){
+    alert('posisi tidak dapat di akses');
+}
+
+ //akses geolocation di function
+function userLocation(){
+    if(!navigator.geolocation){
+        alert('geolocation tidak di dukung didalam browser anda, silahkan gunakan browser lain');
+    }else{
+        navigator.geolocation.getCurrentPosition(success, eror);
+    }
+}
+
+
 function index (){
     let app         = document.getElementById('app');
     let h3          = document.createElement('h3');
     h3.innerHTML    = "player Time";
 
     app.appendChild(h3);
+
+    userLocation();
 }
 
 index();
